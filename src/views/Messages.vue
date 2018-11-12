54c12557e41c08af098a5c593cd1e40d33312e5a
hongbiao <qq@694993708.com>
2018-11-12 22:52:21 +0800

    wancheng

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -3,8 +3,11 @@
     <ul>
       <li v-for="(message,index) in messages" :key="message.id">
         <router-link :to="`/home/messages/detail/${message.id}`">{{message.title}}</router-link>
+        <button @click="pushShow(message.id)">push查看</button>
+        <button @click="replaceShow(message.id)">replace查看</button>
       </li>
     </ul>
+    <button @click="$router.back()">返回</button>
     <hr>
     <router-view/>
   </div>
@@ -26,6 +29,14 @@
         ]
         this.messages = messages
       },500)
+    },
+    methods:{
+      pushShow(id){
+        this.$router.push(`/home/messages/detail/${id}`)
+      },
+      replaceShow(id){
+        this.$router.replace(`/home/messages/detail/${id}`)
+      }
     }
   }
 </script>
